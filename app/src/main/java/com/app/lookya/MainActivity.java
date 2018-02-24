3c8ad9c5c108c4cf3cd20810176022d0c4f2ae7a
Bhadresh Chavada <rujul.co@gmail.com>
2018-02-24 15:10:04 +0530

    first commit

---
+++ app/src/main/java/com/app/lookya/MainActivity.java
@@ -0,0 +1,36 @@
+package com.app.lookya;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.zappar.ZapparEmbed;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                boolean isCompatible = ZapparEmbed.isCompatible(getApplicationContext());
+
+                if (isCompatible) {
+                    Intent i = new Intent(MainActivity.this, ZapparEmbed.getZapcodeClassForIntent());
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "Your device is not compatible to lookya.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }, 3000);
+
+    }
+}
